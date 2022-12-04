ab534a5c63394dd6f999977625f913ba5ecbadff
RostyslavR <rrmail@ukr.net>
2022-12-04 04:30:55 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+import { App } from 'components/App/App';
+import { GlobalStyle } from 'common/GlobalStyle';
+import { theme } from 'common/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
